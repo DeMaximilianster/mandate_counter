148f40d6e9492aa665ffc7012eea9c465150e187
DeMax <trollbol213@gmail.com>
2020-07-27 16:53:34 +0300

    Code clean-up and bug fixes

--- mandate_counter.cpp
+++ mandate_counter.cpp
@@ -10,13 +10,18 @@ using namespace std;
 class Party
 {
 private:
-	string name = "";
-	int votes = 0;
-	int mandates = 0;
-	double support = 0.0;
-	double points = 0.0;
+	string name;
+	int votes;
+	int mandates;
+	double points;
 
 public:
+	Party(){
+		name = "";
+		votes = 0;
+		mandates = 0;
+		points = 0.0;
+	}
 	void setName(string newName) {
 		name = newName;
 	};
@@ -29,14 +34,11 @@ public:
 	int getVotes() {
 		return votes;
 	};
-	void calculateSupport(double totalVotes) {
-		support = round(votes / totalVotes * 100); // support is unchangable attribute 
+	double calculateSupport(double totalVotes) {
+		return round(votes / totalVotes * 100);
 	};
-	double getSupport() {
-		return support;
-	};
-	void clear() {
-		points = support;
+	void clear(double totalVotes) {
+		points = calculateSupport(totalVotes);
 		mandates = 0;
 	};
 	void setPoints(double newPoints) {
@@ -60,33 +62,37 @@ int main(int argc, char** argv)
 {
 	setlocale(0, "rus");
 	system("chcp 1251");
-	int partiesNum, i, j, newVotes, allMandates, pointsForMandate, mandates, leftMandates, points;
+	int partiesNum, newVotes, allMandates, pointsForMandate, mandates, leftMandates, points, partyIndex;
 	// variables for sorting in step 6
 	double sorterValue;
 	int sorterIndex; 
 	double totalVotes = 0.0;
-	string newName;
-	Party parties[15], sortedParties[15], party;
+	string newName, stream;
+	Party party;
 
 	cout << "Шаг 1. Введение данных" << endl;
 	cout << "Введите количество партий на выборах: ";
-	cin >> partiesNum;
+	getline(cin, stream); 
+	partiesNum = stoi(stream);
 
-	for (i = 0; i < partiesNum; i++) {
+	Party *parties = new Party[partiesNum];
+	Party *outputParties = new Party[partiesNum];
+
+	for (int i = 0; i < partiesNum; i++) {
 		cout << "Введите название партии " << i + 1 << ": ";
-		cin >> newName;
+		getline(cin, newName);
 		parties[i].setName(newName);
 		cout << "Введите количество голосов за партию " << i + 1 << ": ";
-		cin >> newVotes;
+		getline(cin, stream);
+		newVotes = stoi(stream);
 		parties[i].setVotes(newVotes);
 		totalVotes += newVotes;
 
 	};
 	cout << "Шаг 2. Вычисление процентов " << endl;
 	cout << "Всего голосов: " << totalVotes << endl;
-	for (i = 0; i < partiesNum; i++) {
-		parties[i].calculateSupport(totalVotes);
-		cout << parties[i].getName() << " - " << parties[i].getSupport() << "%" << endl;
+	for (int i = 0; i < partiesNum; i++) {
+		cout << parties[i].getName() << " - " << parties[i].calculateSupport(totalVotes) << "%" << endl;
 	};
 	cout << "Шаги 3-7. Собственно подсчёт мандатов" << endl;
 	for (allMandates = 5; allMandates <= 100; allMandates++) {
@@ -95,8 +101,8 @@ int main(int argc, char** argv)
 		cout << "Шаг 4. Рассчитываем количество процентов за мандат k=[100/m]=" << pointsForMandate << endl;
 		leftMandates = allMandates;
 		cout << "Шаг 5. Раздаём партиям мандаты за каждое полное число k процентов" << endl;
-		for (i = 0; i < partiesNum; i++) {
-			parties[i].clear();  // start counting mandates
+		for (int i = 0; i < partiesNum; i++) {
+			parties[i].clear(totalVotes);  // start counting mandates
 			// exchange points for mandates
 			points = parties[i].getPoints();
 			mandates = points / pointsForMandate;
@@ -107,41 +113,51 @@ int main(int argc, char** argv)
 			cout << "Партия " << parties[i].getName() << " получает мандатов: " << mandates;
 			cout << " и оставляет очков: " << points << endl;
 		};
-		cout << "Шаг 6. Рассортируем партии по оставшимся очкам" << endl;
-		for (i = 0; i < partiesNum; i++) {
-			sorterValue = -1.0;
-			sorterIndex = 0;
-			for (j = 0; j < partiesNum; j++) {
-				if (parties[j].getPoints() > sorterValue) {
-					sorterValue = parties[j].getPoints();
-					sorterIndex = j;
-				};	
+		if (leftMandates > 0) {
+			cout << "Шаг 6. Рассортируем партии по оставшимся очкам" << endl;
+			for (int i = 0; i < partiesNum; i++) {
+				sorterValue = -1.0;
+				sorterIndex = 0;
+				for (int j = 0; j < partiesNum; j++) {
+					if (parties[j].getPoints() > sorterValue) {
+						sorterValue = parties[j].getPoints();
+						sorterIndex = j;
+					};
+				}
+				outputParties[i] = parties[sorterIndex];
+				parties[sorterIndex].setPoints(-1.0);  // program won't put this party again
 			}
-			sortedParties[i] = parties[sorterIndex];
-			parties[sorterIndex].setPoints(-1.0);  // program won't put this party again
-		}
-		// print sorted array
-		for (i = 0; i < partiesNum; i++) {
-			cout << "Партия " << sortedParties[i].getName() << ". Очков: " << sortedParties[i].getPoints();
+			// print sorted array
+			for (int i = 0; i < partiesNum; i++) {
+				cout << "Партия " << outputParties[i].getName() << ". Очков: " << outputParties[i].getPoints();
+				cout << endl;
+			};
+			cout << "Шаг 7. Распределим между партиями оставшиеся мандаты в количестве " << leftMandates;
 			cout << endl;
-		};
-		cout << "Шаг 7. Распределим между партиями оставшиеся мандаты в количестве " << leftMandates;
-		cout << endl;
-		for (i = 0; i < leftMandates; i++) {
-			sortedParties[i].addMandate();
-			cout << "Партия " << sortedParties[i].getName() << " получила мандат" << endl;
+			for (partyIndex= 0; partyIndex < leftMandates; partyIndex++) {
+				outputParties[partyIndex].addMandate();
+				cout << "Партия " << outputParties[partyIndex].getName() << " получила мандат" << endl;
+			}
+			if (outputParties[partyIndex].getPoints() == outputParties[partyIndex - 1].getPoints()) {
+				cout << "Распределение не вышло, возвращаемся к шагу 3" << endl;
+			}
+			else {
+				cout << "Распределение успешно" << endl;
+				break;
+			}
 		}
-		if (sortedParties[i].getPoints() == sortedParties[i - 1].getPoints()) {
-			cout << "Распределение не вышло, возвращаемся к шагу 3" << endl;
-		}
-		else{
-			cout << "Распределение успешно" << endl;
+		else {
+			cout << "Всё сошлось идеально, так что пропускаем шаги 6 и 7 ";
+			cout << "(сортировка партий и распределение мандатов)" << endl;
+			for (int i = 0; i < partiesNum; i++) {
+				outputParties[i] = parties[i];
+			}
 			break;
 		}
 	};
 	cout << "Шаг 8. Печать результатов" << endl;
-	for (i = 0; i < partiesNum; i++) {
-		cout << sortedParties[i].getName() << ". Мандаты: " << sortedParties[i].getMandates() << endl;
+	for (int i = 0; i < partiesNum; i++) {
+		cout << outputParties[i].getName() << ". Мандаты: " << outputParties[i].getMandates() << endl;
 	}
 	return 0;
 }
